f21415e9592d5c12ca14ddaeea6240e48276ff3f
Evandri Ridho Hasmono <evandriridho321@gmail.com>
2024-02-07 23:38:22 +0700

    Challange WPU hari ke 001

--- 001-Grow/reduceButGrow.js
+++ 001-Grow/reduceButGrow.js
@@ -2,7 +2,7 @@
 // 1/366
 // https://www.codewars.com/kata/57f780909f7e8e3183000078
 
-function grow(x) {
+function grow(x) { // Cara Basic menggunakan For
     let result = x[0];
     for(let i = 1; i < x.length; i++) {
         result *= x[i];
@@ -10,6 +10,5 @@ function grow(x) {
     return result;
 }
 
-const grown = (x) => ((acc,value) => acc * values);
-
+const grown = (m) => ((acc,value) => acc * value); // Cara Advanced menggunakan reduce
 console.log([2,2,2,2,2,]); 
