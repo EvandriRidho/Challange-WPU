24dc329e8367a6732fdff74a0cf2594ead920747
Evandri Ridho Hasmono <evandriridho321@gmail.com>
2024-02-07 23:21:57 +0700

    Challange ke 001

---
+++ 001-Grow/reduceButGrow.js
@@ -0,0 +1,15 @@
+// WPU Coding Challange 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+function grow(x) {
+    let result = x[0];
+    for(let i = 1; i < x.length; i++) {
+        result *= x[i];
+    }
+    return result;
+}
+
+const grown = (x) => ((acc,value) => acc * values);
+
+console.log([2,2,2,2,2,]); 
